c5f740e2aaa5719cbc12a636662e6af52b333a1b
Matthew Chun <matthewchun93@gmail.com>
2021-02-16 08:02:29 -0800

    todo incomplete

--- week14/2-Tuesday/labs/shopping_cart_prep/src/index.js
+++ week14/2-Tuesday/labs/shopping_cart_prep/src/index.js
@@ -4,26 +4,34 @@ import App from './App';
 import Cart from './components/Cart';
 import Products from './components/Products'
 import BaseLayout from './components/layout/BaseLayout'
+import {Provider} from 'react-redux';
+
 import {
   BrowserRouter as Router,
   Route, Switch
 } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+//redux
+import {createStore} from 'redux'; //Redux.createStore()
+import {Provider} from 'react-redux';
 
+let store = createStore() //initialize redux-reducer
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <BaseLayout>
-        <Switch>
-          <Route exact path='/' component={App}/>
-          <Route path='/cart' component={Cart}/>
-          <Route path='/products' component={Products}/>
-          
-        </Switch>
-      </BaseLayout>
-    </Router>
+    <Provider>
+      <Router>
+        <BaseLayout>
+          <Switch>
+            <Route exact path='/' component={App}/>
+            <Route path='/cart' component={Cart}/>
+            <Route path='/products' component={Products}/>
+            
+          </Switch>
+        </BaseLayout>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
