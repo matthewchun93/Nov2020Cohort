06a020486f99fc7062d387c42663c1469b9bd600
Veronica Lino <veronica@digitalcrafts.com>
2021-02-17 20:29:54 -0600

    todo

---
+++ week14/4-Thursday/veros-notes-dont-touch/todo_prep/src/components/TodoItem.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {useDispatch, useSelector} from 'react-redux';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { SpanBadge, ShowHideDiv } from "./Styles";
+import TodoForm from './TodoForm'
+import {deleteTodo, setEditMode} from '../actions/index'
+
+
+const TodoItem = ({todo}) => {
+  
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <li>
+        <div className="row pr-3">
+          <div className="col-10">
+            {
+              todo.editMode
+              ?
+              <TodoForm todo={todo} editMode={todo.editMode} pageType={1} /> 
+              :
+              
+              todo.task
+             
+            }
+            
+          </div>
+          {/* end of col div for text */}
+
+          <div className="col-2 text-right pr-2">
+
+              <button className="button btn" onClick={()=>dispatch(setEditMode(todo.id, true))} >
+                <SpanBadge>
+                    <FontAwesomeIcon icon={["fas", "pencil-alt"]} color="gray" />
+                </SpanBadge>
+              </button>
+
+              <button className="button btn" onClick={()=>dispatch(deleteTodo(todo.id))}>
+                <SpanBadge>
+                    <FontAwesomeIcon icon={["fas", "trash"]} color="gray" />
+                </SpanBadge>
+              </button>
+             
+          </div>
+          {/* end of col div for buttons */}
+        </div>
+
+      </li>
+    </>
+  );
+};
+
+export default TodoItem;
