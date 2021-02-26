244fec4932cb54143e36d4bc531e094aad8e79a5
Veronica Lino <veronica@digitalcrafts.com>
2021-02-26 11:10:39 -0600

    jwt

---
+++ week15/5-Friday/veros-notes-dont-touch/auth/server/app.js
@@ -0,0 +1,36 @@
+
+const express = require('express');
+const app = express();
+
+app.use(require('./routes/authentications'));
+
+app.listen(3001, ()=>{
+    console.log(`listening on port 3001`);
+})
+
+
+// const jwt = require('jwt-simple'); // create a jwt token
+
+// user information 
+
+// let userInfo = {
+//     id: '12345',
+//     userName: 'Veronica',
+//     email: 'me@me.com'
+// }
+
+//create a function that returns a jwt token
+
+// const token = (user) => {
+
+//     let timestamp = new Date().getTime();  //current time
+    
+//     return jwt.encode({sub: user.id, name: user.userName, iat:timestamp}, "lskjdfls;j;lfkajdljlds"); //encode creates tokent
+// }
+
+/// call function and pass our user 
+
+// let jwtToken = token(userInfo);
+
+// console.log(jwtToken);
+
